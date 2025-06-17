ad9e28f082a88c6720347fe42bcb9e86e64d5bcf
Johan Ekenstedt <johan.ekenstedt@arboreal.se>
2025-06-17 15:27:43 +0200

    Initial Commit

---
+++ BluetoothScanner/BluetoothScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluetoothScannerApp.swift
+//  BluetoothScanner
+//
+//  Created by Johan Ekenstedt on 2025-06-17.
+//
+
+import SwiftUI
+
+@main
+struct BluetoothScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
